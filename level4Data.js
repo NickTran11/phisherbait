8fd4f0a7bc8e7c5cfefc99cdf12947052146a768
NickTran11 <nicktran112006@gmail.com>
2026-04-16 00:23:26 -0600

    Update level4Data.js

--- level4Data.js
+++ level4Data.js
@@ -4,7 +4,7 @@ window.LEVEL4_EMAIL = {
   scenario: {
     codename: "THEO BUCH",
     title: "Microsoft Employee",
-    description: "You are Theo Buch. Level 4 introduces clone phishing. Some emails are legitimate workplace messages, while others copy familiar formatting but use slightly changed domains, reply-style subjects, or swapped malicious links.",
+    description: "You are Theo Buch. Level 4 introduces clone phishing. Some emails are legitimate workplace messages, while others copy familiar formatting but use slightly changed domains, reply-style subjects.",
     profile: [
       "Works in a Microsoft office environment",
       "Receives invoices, document shares & IT alerts",
